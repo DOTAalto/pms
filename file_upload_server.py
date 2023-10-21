ec0d3553e09bd684d7b087ca8be1004023868278
Felix Bade <flix.bade@gmail.com>
2023-10-21 13:16:25 +0300

    improve form

--- file_upload_server.py
+++ file_upload_server.py
@@ -1,29 +1,30 @@
 from flask import Flask, request, render_template
 import os
-import uuid
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './uploads'
+app.config['COUNTER'] = 0 # initial counter
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        unique_id = uuid.uuid4().hex
-        path = os.path.join(app.config['UPLOAD_FOLDER'], unique_id)
+        app.config['COUNTER'] += 1
+        path = os.path.join(app.config['UPLOAD_FOLDER'], str(app.config['COUNTER']))
         if not os.path.exists(path):
             os.makedirs(path)
-        
+
         file = request.files['file']
+        name = request.form.get('name')
         author = request.form.get('author')
         description = request.form.get('description')
-        
+
         file.save(os.path.join(path, file.filename))
-        
+
         with open(os.path.join(path, "info.txt"), "w") as info:
-            info.write(f"Author: {author}\nDescription: {description}")
-            
+            info.write(f"Name: {name}\nAuthor: {author}\nDescription: {description}")
+
         return 'File upload successfully!'
     return render_template('upload.html')
 
