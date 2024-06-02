7c4e70b6221b0aa5c5fbd8e09f8c3026690b4e2a
Joel Juhela <joel.juhela@protonmail.com>
2024-06-02 23:46:45 +0300

    Enforce deadlines in form clean for entry
    closes #14

--- party/forms.py
+++ party/forms.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm, HiddenInput
+from django.utils import timezone
 
 from party.models import Entry
 
@@ -16,3 +18,22 @@ class EntryForm(ModelForm):
             'team': 'Author',
             'sub_file': 'File'
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        compo = cleaned_data['compo'] 
+        now = timezone.now()
+        
+        # check for submission_deadline and metadata_deadline
+        initial_sub_file = self.initial.get('sub_file')
+        cleaned_sub_file = self.cleaned_data.get('sub_file')
+
+        changed_data = self.changed_data
+        if 'sub_file' in self.changed_data and now > compo.submission_deadline:
+            raise ValidationError("The deadline for submitting a file has passed. You can still edit the other information of your entry. If your issue is critical, please contact the organizers")
+
+        
+        if now > compo.metadata_deadline:
+            raise ValidationError("The deadline for the compo has passed, you can't edit or submit a new entry. If your issue is critical, please contact the organizers")
+        
+        return cleaned_data
