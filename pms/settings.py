cd4186493339bae88594d4477e6bbeff0523a182
Joel Juhela <joel.juhela@protonmail.com>
2024-06-06 07:17:32 +0300

    Add STATIC_ROOT to settings

--- pms/settings.py
+++ pms/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -114,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 MEDIA_URL = 'uploads/'
 MEDIA_ROOT = 'uploads/'
 
