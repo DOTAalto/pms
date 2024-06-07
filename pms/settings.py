4ccc3dfc163fc79c281d6114012140d16fab47fa
Joel Juhela <joel.juhela@protonmail.com>
2024-06-07 21:12:40 +0300

    Read SECRET_KEY, ALLOWED_HOSTS and DEBUG from env

--- pms/settings.py
+++ pms/settings.py
@@ -21,6 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 
+DEBUG = os.environ.get("DEBUG", 0)
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
+
 
 # Application definition
 
